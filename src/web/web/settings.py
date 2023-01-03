1a2f0b974d789d73b10850060038630b5f01be4b
Lukáš Kucharczyk <lukas@kucharczyk.xyz>
2023-01-03 20:41:26 +0100

    Set timezone from TZ env variable

--- src/web/web/settings.py
+++ src/web/web/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import logging
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -107,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = os.environ["TZ"]
 
 USE_I18N = True
 
