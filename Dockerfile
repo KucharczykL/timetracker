1ccfdc321aaf98062db0b5d3d4d200f70a26b105
Lukáš Kucharczyk <lukas@kucharczyk.xyz>
2023-01-07 21:59:17 +0100

    Start caddy in the background

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ RUN npm install && \
 
 FROM python:3.10.9-alpine
 
-ENV VERSION_NUMBER 0.1.1-5-g270d9f7
+ENV VERSION_NUMBER 0.1.1-6-g25a58c2
 ENV PROD 1
 
 RUN apk add \
--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,6 @@ echo "Collect static files"
 poetry run python src/web/manage.py collectstatic
 
 echo "Starting server"
-caddy run
+caddy start
 cd src/web || exit
 poetry run python -m gunicorn --bind 0.0.0.0:8001 web.asgi:application -k uvicorn.workers.UvicornWorker
