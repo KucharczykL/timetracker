d029fda8964f404eb9af96756ad738653a41cb81
Lukáš Kucharczyk <lukas@kucharczyk.xyz>
2023-01-07 18:10:36 +0100

    Collect static files in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,9 @@
 echo "Apply database migrations"
 poetry run python src/web/manage.py migrate
 
+echo "Collect static files"
+poetry run python src/web/manage.py collectstatic
+
 # Start server
 echo "Starting server"
 poetry run python src/web/manage.py runserver 0.0.0.0:8000
