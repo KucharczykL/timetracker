0d20b543b0294dcf5ee160419ba780eaa9e3120e
Lukáš Kucharczyk <lukas@kucharczyk.xyz>
2023-01-07 21:59:54 +0100

    Do not load the admin interface in prod

--- src/web/web/urls.py
+++ src/web/web/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
+from django.conf import settings
 
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("", RedirectView.as_view(url="/tracker/list-sessions")),
     path("tracker/", include("tracker.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns.append(path("admin/", admin.site.urls))
