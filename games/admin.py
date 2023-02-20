366c25a1ff2ef871eb3406edeae549dd143af173
Lukáš Kucharczyk <lukas@kucharczyk.xyz>
2023-02-20 22:01:20 +0100

    Register Edition, Device in the admin UI

--- games/admin.py
+++ games/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
-from games.models import Game, Platform, Purchase, Session
+from games.models import Game, Platform, Purchase, Session, Edition, Device
 
 # Register your models here.
 admin.site.register(Game)
 admin.site.register(Purchase)
 admin.site.register(Platform)
 admin.site.register(Session)
+admin.site.register(Edition)
+admin.site.register(Device)
