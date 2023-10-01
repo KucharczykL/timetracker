787ee8640f63d536e44ba9e6a99cbb9c17949fee
Lukáš Kucharczyk <lukas@kucharczyk.xyz>
2023-10-01 19:57:15 +0200

    Try to shutdown container gracefully and faster

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,13 @@ poetry run python manage.py migrate
 echo "Collect static files"
 poetry run python manage.py collectstatic --clear --no-input
 
+_term() {
+  echo "Caught SIGTERM signal!"
+  kill -SIGTERM "$gunicorn_pid"
+}
+trap _term SIGTERM
+
 echo "Starting app"
-poetry run python -m gunicorn --bind 0.0.0.0:8001 timetracker.asgi:application -k uvicorn.workers.UvicornWorker --access-logfile - --error-logfile -
+poetry run python -m gunicorn --bind 0.0.0.0:8001 timetracker.asgi:application -k uvicorn.workers.UvicornWorker --access-logfile - --error-logfile - & gunicorn_pid=$!
+
+wait "$gunicorn_pid"
