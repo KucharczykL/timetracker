7dc43fbf7701ffab56472d4f98e9aeb49a95fd46
Lukáš Kucharczyk <lukas@kucharczyk.xyz>
2023-11-20 21:54:51 +0100

    Fix wrong export name

--- games/static/js/utils.js
+++ games/static/js/utils.js
@@ -201,7 +201,7 @@ export {
   syncSelectInputUntilChanged,
   getEl,
   conditionalElementHandler,
-  disableElementsWhenFalse,
+  disableElementsWhenValueNotEqual,
   disableElementsWhenTrue,
   getValueFromProperty,
 };
