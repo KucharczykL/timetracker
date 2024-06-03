cd804f2c7712a1563889b53c8f70ef6aa8d838ec
Lukáš Kucharczyk <lukas@kucharczyk.xyz>
2024-06-03 18:18:58 +0200

    Sort url paths

--- timetracker/urls.py
+++ timetracker/urls.py
@@ -23,10 +23,10 @@ from graphene_django.views import GraphQLView
 
 urlpatterns = [
     path("", RedirectView.as_view(url="/tracker")),
+    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("tracker/", include("games.urls")),
-    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
 
 if settings.DEBUG:
