8cb67ca00281177ab8d2fcd4ba1fba9bf311f736
Lukáš Kucharczyk <lukas@kucharczyk.xyz>
2025-03-17 08:36:41 +0100

    Add updated_at to Game

--- games/models.py
+++ games/models.py
@@ -22,6 +22,7 @@ class Game(models.Model):
     )
 
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Status(models.TextChoices):
         UNPLAYED = (
